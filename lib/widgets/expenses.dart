a0d0c4bde0639a657677aa133f0cde183a86895f
MarceloBill84 <marcelobill@msn.com>
2024-11-08 17:12:43 -0300

    Implementing new expense widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,9 +30,16 @@ class _ExpensesState extends State<Expenses> {
 
   void openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) => const NewExpense(),
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
     );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);      
+    });
   }
 
   @override
